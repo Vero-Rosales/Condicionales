48405c04750676e388b880e1617d8e98454ccfb3
Verónica Rosales <veronica.rosales@ucuenca.edu.ec>
2024-09-30 17:09:30 -0500

    se hicieron más modificaciones

--- sketch.js
+++ sketch.js
@@ -9,16 +9,15 @@ function draw() {
   } else {
     fill("black");
   }
-  drawHeart(mouseX, mouseY, 50); // Dibuja un corazón en lugar de un círculo
+  drawHeart(mouseX, mouseY, 50);
 }
 
-// Función para dibujar un corazón
 function drawHeart(x, y, size) {
   beginShape();
   vertex(x, y);
-  // Parte izquierda del corazón
+
   bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
-  // Parte derecha del corazón
+
   bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
   endShape(CLOSE);
 }
