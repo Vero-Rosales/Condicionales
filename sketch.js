6c4ecfb028f8f005bd47ef294b9fa2f1b785275d
Verónica Rosales <veronica.rosales@ucuenca.edu.ec>
2024-09-30 16:15:35 -0500

    se creó un nuevo condicional

--- sketch.js
+++ sketch.js
@@ -1,6 +1,28 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(150, 0, 150, 150);
 }
 
-function draw() {}
+function draw() {
+  if (mouseIsPressed) {
+    fill("white");
+  } else {
+    fill("black");
+  }
+  drawStar(mouseX, mouseY, 30, 70, 5);
+}
+
+function drawStar(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
